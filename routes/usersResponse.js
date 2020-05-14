b1dc020118086e2be753dd16452428f1190531c1
rahuljain46 <rahuljain820436@gmail.com>
2020-05-15 00:29:57 +0530

    feedback response

--- routes/usersResponse.js
+++ routes/usersResponse.js
@@ -21,86 +21,84 @@ userReponseRouter
       var collection = db.collection(userResponseCollection);
       var query = req.query;
       if (
-        !(Object.keys(query).length === 0 && query.constructor === Object) && query.allresult != undefined &&
+        !(Object.keys(query).length === 0 && query.constructor === Object) &&
+        query.allresult != undefined &&
         query.allresult === "true"
       ) {
-        
         db.collection("users")
           .aggregate([
             {
-              $lookup:{
-        
-             from: "usersresponse",
-        
-            let: { user_Id: "$userId" },
-        
-            pipeline: [
-        
-              {
-        
-                $match: {
-                  $expr: {
-                      $and:
-                          [
-                      {$in: ["$$user_Id","$usersAnswer.userId"]},
-                      {$eq: ["$date",query.date]}
-                          ]
+              $lookup: {
+                from: "usersresponse",
+
+                let: { user_Id: "$userId" },
+
+                pipeline: [
+                  {
+                    $match: {
+                      $expr: {
+                        $and: [
+                          { $in: ["$$user_Id", "$usersAnswer.userId"] },
+                          { $eq: ["$date", query.date] }
+                        ]
+                      }
+                    }
+                  },
+
+                  {
+                    $project: {
+                      "usersAnswer.userId": 1,
+                      "usersAnswer.score": 1,
+                      "usersAnswer.time": 1,
+                      "usersAnswer.feedback": 1,
+                      "usersAnswer.suggestion": 1,
+                      date: 1
+                    }
                   }
-                }
-        
-              },
-        
-              {
-        
-                $project: {
-                  "usersAnswer.userId": 1,
-                  "usersAnswer.score": 1,
-                  "usersAnswer.time": 1,
-                  date: 1
-        
-                }
-        
+                ],
+                as: "userInfo"
               }
-        
-            ],
-            as: "userInfo"
-        
             }
-        
-                }
           ])
           .toArray(function(err, results) {
             if (err) throw err;
-            let ObjArray =[]
-            results.map((result)=>{
+            let ObjArray = [];
+            results.map(result => {
               let id = result.userId;
-              let obj ={}
-              if(result.userInfo.length!=0){
-                result.userInfo[0].usersAnswer.map((useranswer)=>{
-                  if(useranswer.userId === id){
+              let obj = {};
+              if (result.userInfo.length != 0) {
+                result.userInfo[0].usersAnswer.map(useranswer => {
+                  if (useranswer.userId === id) {
                     obj["time"] = useranswer.time;
                     obj["score"] = useranswer.score;
+                    obj["feedback"] = useranswer.feedback;
+                    obj["suggestion"] = useranswer.suggestion;
                   }
-                })
-                obj["fullname"]=result.fullname;
-                obj["city"]=result.city;
-                ObjArray.push(obj)
+                });
+                obj["fullname"] = result.fullname;
+                obj["city"] = result.city;
+                ObjArray.push(obj);
               }
-            })
+            });
             res.json(ObjArray);
             db.close();
           });
-      }else if(
+      } else if (
         !(Object.keys(query).length === 0 && query.constructor === Object) &&
-        query.date != undefined && query.userId != undefined
-      ){
-        collection.find( {date: query.date, "usersAnswer.userId": query.userId}).count( {}, function(err, results) {
-          let resp = results;
-          res.json(resp);
-          db.close();
-
-      });
-    }else if (!(Object.keys(query).length === 0 && query.constructor === Object) && query) {
+        query.date != undefined &&
+        query.userId != undefined
+      ) {
+        collection
+          .find({ date: query.date, "usersAnswer.userId": query.userId })
+          .count({}, function(err, results) {
+            let resp = results;
+            res.json(resp);
+            db.close();
+          });
+      } else if (
+        !(Object.keys(query).length === 0 && query.constructor === Object) &&
+        query
+      ) {
         collection.find(query).toArray(function(err, results) {
           let resp = results;
           res.json(resp);
@@ -135,31 +133,37 @@ userReponseRouter
         db.close();
       });
     });
-  }).patch(function(req,res){
+  })
+  .patch(function(req, res) {
     mongodb.connect(connectionString, function(err, db) {
       if (err) {
         console.log(err);
         return;
       }
       var usersResponse = req.body;
-     
+
       var collection = db.collection(userResponseCollection);
-      
-      let query = req.query
-      if( !(Object.keys(query).length === 0 && query.constructor === Object) 
-         && query.date != null  && query.date != undefined 
-      && query.update === "true" && query.update != undefined ){
-        collection.update( {date: query.date}, { $push: {usersAnswer:usersResponse}  } 
-        , function(err, results) {
-         console.log(results);
-         res.send("update is successful " + results.result.ok);
-         db.close();
-       });
 
+      let query = req.query;
+      if (
+        !(Object.keys(query).length === 0 && query.constructor === Object) &&
+        query.date != null &&
+        query.date != undefined &&
+        query.update === "true" &&
+        query.update != undefined
+      ) {
+        collection.update(
+          { date: query.date },
+          { $push: { usersAnswer: usersResponse } },
+          function(err, results) {
+            console.log(results);
+            res.send("update is successful " + results.result.ok);
+            db.close();
+          }
+        );
       }
-
     });
-  })
+  });
 
 userReponseRouter
   .route("/:id")
